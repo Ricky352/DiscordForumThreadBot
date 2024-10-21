65e6c353ba39ebd02290267b8de668f5e2acadf4
Ricky352 <lopes.ricky97@gmail.com>
2024-10-21 08:50:54 +0200

    Updated bot to add tags to a thread

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 FORUM_CHANNEL_ID = int(os.getenv('FORUM_CHANNEL_ID'))
+tag_id = int(os.getenv('TAG_ID'))
 
 intents = discord.Intents.default()
 intents.guilds = True
@@ -17,9 +18,25 @@ client = discord.Client(intents=intents)
 
 async def create_thread_in_forum():
     now = datetime.now()
+    forum_channel = client.get_channel(FORUM_CHANNEL_ID)
+    threads = forum_channel.threads
+    threads_with_tag = [
+        thread for thread in threads if any(tag.id == tag_id for tag in thread.applied_tags)
+    ]
+    thread_url = ""
+
+    if threads_with_tag:
+        latest_thread = sorted(threads_with_tag, key=lambda t: t.created_at, reverse=True)[0]
+        thread_url = f"https://discord.com/channels/{forum_channel.guild.id}/{latest_thread.id}"
+        print(f"Latest thread with tag ID {tag_id}: {thread_url}")
+    else:
+        print(f"No threads found with tag ID {tag_id}.")
+
     thread_name = f"Daily Write Down {now.strftime('%d/%m/%Y')}"
     thread_content = (
         "<@&1049215981349781566> <@&1049216330647228436>\n\n"
+        f"Previous: {thread_url}\n\n"
+        "Please answer the following questions:\n"
         "```\n"
         "### What you have done yesterday\n"
         "{{ Answer here }}\n"
@@ -29,11 +46,13 @@ async def create_thread_in_forum():
         "{{ Answer here }}\n"
         "```"
     )
-    forum_channel = client.get_channel(FORUM_CHANNEL_ID)
+
     if isinstance(forum_channel, discord.ForumChannel):
+        tag_to_apply = next((tag for tag in forum_channel.available_tags if tag.id == tag_id), None)
         await forum_channel.create_thread(
             name=thread_name,
-            content=thread_content
+            content=thread_content,
+            applied_tags=[tag_to_apply]
         )
         print(f"Created thread: {thread_name}")
 
